3dbd7d13c652e896f87621d7c39c0227a7e000e5
Maxime Bélanger De Blois <maxime.belangerdeblois@mcgill.ca>
2024-05-15 13:35:39 -0400

    addition of explore graph in context

--- R/api_context.R
+++ R/api_context.R
@@ -5,19 +5,21 @@ region = NULL
 time <- 2021
 select_id <- NA
 schema <- "mtl"
+lang <- NULL
 
-context <- function(conn, var_left, var_right = " ", scale, region = NULL, time, select_id) {
+context <- function(conn, var_left, var_right = " ", scale, region = NULL, time, select_id,
+                    lang = NULL) {
 
   variables <- db_get(select = "*", from = "variables", schema = schema,
                       where = list(var_code = c(var_left, var_right)))
-  variables$schema <- list(time = "_\\d{4}$")
-  variables$breaks_var <- NA
 
   vars <- vars_build(var_left, var_right = " ", scale, time, variables = variables)
   time_formatted <- vars$time
   vars <- vars$vars
   data <- data_get(vars, scale, region, variables = variables)
 
-  legend_render(vars = vars, scale = scale, data = data, variables = variables)
+  legend <- legend_render(vars = vars, scale = scale, data = data, variables = variables)
+  explore_graph(vars = vars, select_id = select_id, scale = scale, data = data,
+                time = time, schemas = schemas, lang = lang, variables = variables)
 
 }
